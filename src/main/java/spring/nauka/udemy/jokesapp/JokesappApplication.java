d74ca0abfabc5d6a33ab97816a2fb71a813b3754
JSzyper <jacek.szyper@savangard.com>
2020-09-17 15:25:43 +0200

    initial commit

---
+++ src/main/java/spring/nauka/udemy/jokesapp/JokesappApplication.java
@@ -0,0 +1,13 @@
+package spring.nauka.udemy.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/nauka/udemy/jokesapp/JokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.nauka.udemy.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
