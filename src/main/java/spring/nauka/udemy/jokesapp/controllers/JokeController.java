31c4e65a35286d9d1fa5282d023002cd84991fcd
JSzyper <jacek.szyper@savangard.com>
2020-09-18 16:58:05 +0200

    added service, controller, thymeleaf and banner

---
+++ src/main/java/spring/nauka/udemy/jokesapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package spring.nauka.udemy.jokesapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.nauka.udemy.jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+    }
+
+}
---
+++ src/main/java/spring/nauka/udemy/jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package spring.nauka.udemy.jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
